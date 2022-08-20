9251b7dec4cce10a702c373f1df60951b9e05d58
zangxin <czangxin@foxmail.com>
2022-08-20 13:24:27 +0800

    第三次提交,加入后台管理系统的前端部分

---
+++ module-management-web-view/module-web-view-admin/src/api/product/sku.js
@@ -0,0 +1,36 @@
+import request from '@/utils/request'
+
+export default {
+
+  getPageList(page, limit) {
+    return request({
+      url: `/admin/product/list/${page}/${limit}`,
+      method: 'get'
+    })
+  },
+
+  // 保存Sku
+  saveSkuInfo(skuForm) {
+    return request({
+      url: '/admin/product/saveSkuInfo',
+      method: 'post',
+      data: skuForm
+    })
+  },
+
+  // 商品上架
+  onSale(skuId) {
+    return request({
+      url: `/admin/product/onSale/${skuId}`,
+      method: 'get'
+    })
+  },
+
+  // 商品下架
+  cancelSale(skuId) {
+    return request({
+      url: `/admin/product/cancelSale/${skuId}`,
+      method: 'get'
+    })
+  }
+}
